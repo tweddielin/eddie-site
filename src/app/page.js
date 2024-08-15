a516e0365acb422689203809b3babb92da08fe1c
Eddie Lin <tweddielin@gmail.com>
2024-08-15 15:26:18 -0400

    add favicon and socials

--- src/app/page.js
+++ src/app/page.js
@@ -16,9 +16,9 @@ export default function Home() {
       <div className="flex items-center space-x-4 mb-4">
         <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">Eddie</h1>
         <div className="flex items-center space-x-4 mb-6">
-          <SocialIcon href="https://www.instagram.com/yourinstagram" icon={FaInstagram} label="Instagram" />
-          <SocialIcon href="https://soundcloud.com/yoursoundcloud" icon={FaSoundcloud} label="SoundCloud" />
-          <SocialIcon href="https://open.spotify.com/artist/yourspotifyid" icon={FaSpotify} label="Spotify" />
+          <SocialIcon href="https://www.instagram.com/tweddielin" icon={FaInstagram} label="Instagram" />
+          <SocialIcon href="https://soundcloud.com/tweddielin" icon={FaSoundcloud} label="SoundCloud" />
+          <SocialIcon href="https://open.spotify.com/artist/tweddielin" icon={FaSpotify} label="Spotify" />
         </div>
       </div>
         <p className="text-xl">Keyboardist • Beatmaker • Producer</p>
