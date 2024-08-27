06aed532fde4c68dd7a9535226a85bc987f74cfb
Eddie Lin <tweddielin@gmail.com>
2024-08-27 15:58:52 -0400

    add video

--- src/app/music/page.js
+++ src/app/music/page.js
@@ -2,7 +2,18 @@ import Image from 'next/image'
 
 export default function Music() {
   const albums = [
-    { title: 'Superposition State', year: '2024', cover: '/images/superposition-state-album-cover.png' },
+    { 
+      title: 'Superposition State', 
+      year: '2024', 
+      cover: '/images/superposition-state-album-cover.png', 
+      type: 'image' 
+    },
+    { 
+      title: 'Talk (Khalid) Cover - Live from Narwhal Studios', 
+      year: '2021', 
+      cover: 'https://www.youtube.com/embed/pVdzklaS9mE?si=YTIlmbZWmTdnwyBQ', 
+      type: 'youtube'
+    }
     // Add more albums as needed
   ]
   return (
@@ -11,14 +22,27 @@ export default function Music() {
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {albums.map((album, index) => (
           <div key={index} className="text-center">
-            <div className="relative w-64 h-64 mx-auto"> {/* This creates a square aspect ratio */}
-              <Image
-                src={album.cover}
-                alt={album.title}
-                layout="fill"
-                objectFit="cover"
-                className="rounded-lg"
-              />
+            <div className="relative w-64 h-64 mx-auto">
+              {album.type === 'image' ? (
+                <Image
+                  src={album.cover}
+                  alt={album.title}
+                  fill
+                  style={{ objectFit: "cover" }}
+                  className="rounded-lg"
+                />
+              ) : (
+                <iframe
+                  width="256"
+                  height="256"
+                  src={album.cover}
+                  title={album.title}
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="rounded-lg"
+                ></iframe>
+              )}
             </div>
             <h2 className="text-xl font-semibold mt-4">{album.title}</h2>
             <p className="text-gray-400">{album.year}</p>
